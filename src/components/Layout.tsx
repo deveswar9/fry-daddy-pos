2871eb3ae4b93ffa866497bf04f6e212854ad3e4
Eswar Reddy <amuleswarareddy@gmail.com>
2026-07-20 08:55:34 +0530

    Fix navigation double-click bug by removing AnimatePresence mode=wait around Outlet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -334,17 +334,7 @@ export const Layout: React.FC = () => {
         </header>
 
         <main className="flex-1 max-w-7xl w-full mx-auto p-4 sm:p-6 lg:p-8">
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={location.pathname}
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.2 }}
-            >
-              <Outlet />
-            </motion.div>
-          </AnimatePresence>
+          <Outlet />
         </main>
       </div>
 
